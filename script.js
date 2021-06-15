9acf7e851bbe3687a9903aef44c8889f1a1d35c6
tan27 <tn2706@gmail.com>
2021-06-15 13:29:05 -0700

    arrangements, css grid

---
+++ script.js
@@ -0,0 +1,34 @@
+var sticky = document.getElementById("sticky");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav-links");
+// const bars = document.querySelector(".fa-bars");
+
+window.onscroll = function() {scrollFunction()};
+sticky.addEventListener('click', topFunction)
+
+hamburger.addEventListener("click", mobileMenu);
+navLinks.forEach(n => n.addEventListener("click", closeMenu));
+bars.addEventListener('click', hamFunction);
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    sticky.style.display = "block";
+  } else {
+    sticky.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
+
+function mobileMenu() {
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    navMenu.classList.remove("active");
+}
