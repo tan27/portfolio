c2e7ad7cd312bf2a87069bb037e835bbe3976788
tan27 <tn2706@gmail.com>
2021-07-07 23:53:12 -0700

    design and function change

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
-var sticky = document.getElementById("sticky");
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-links");
-const navLinks = document.querySelectorAll(".nav-links");
-const bars = document.querySelector(".fa-bars");
+// var sticky = document.getElementById("sticky");
+var nav = document.getElementById("navi");
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-links");
+// const navLinks = document.querySelectorAll(".nav-links");
+// const bars = document.querySelector(".fa-bars");
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrolling()};
 sticky.addEventListener('click', topFunction)
 
-hamburger.addEventListener("click", mobileMenu);
-navLinks.forEach(n => n.addEventListener("click", closeMenu));
-bars.addEventListener('click', hamFunction);
+// hamburger.addEventListener("click", mobileMenu);
+// navLinks.forEach(n => n.addEventListener("click", closeMenu));
+// bars.addEventListener('click', hamFunction);
 
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    sticky.style.display = "block";
+function scrolling() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    nav.style.display = "inline";
   } else {
-    sticky.style.display = "none";
+    nav.style.display = "none";
   }
 }
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     sticky.style.display = "block";
+//   } else {
+//     sticky.style.display = "none";
+//   }
+// }
 
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0; 
 }
 
-function mobileMenu() {
-    navMenu.classList.toggle("active");
-    bars.classList.toggle('fa-bars');
-}
+// function mobileMenu() {
+//     nav = navMenu.classList.toggle("active");
+//     bars.classList.toggle('fa-bars');
+// }
 
-function hamFunction() {
-  bars.classList.toggle('fa-times');
+// function hamFunction() {
+//   bars.classList.toggle('fa-times');
+// }
 
-}
-
-function closeMenu() {
-    navMenu.classList.remove("active");
-}
+// function closeMenu() {
+//     navMenu.classList.remove("active");
+// }
