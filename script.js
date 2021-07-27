e9d0a8fa1f7840f8df7aecbd5f6d7135a9391522
tan27 <tn2706@gmail.com>
2021-07-27 14:40:39 -0700

    cleanup

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-// var sticky = document.getElementById("sticky");
 var nav = document.getElementById("navi");
 var aside = document.getElementById("aside")
-// const hamburger = document.querySelector(".hamburger");
-// const navMenu = document.querySelector(".nav-links");
-// const navLinks = document.querySelectorAll(".nav-links");
-// const bars = document.querySelector(".fa-bars");
 
-// window.onscroll = function() {scrollFunction()};
 window.onscroll = function() {scrolling()};
-// sticky.addEventListener('click', topFunction)
-
-// hamburger.addEventListener("click", mobileMenu);
-// navLinks.forEach(n => n.addEventListener("click", closeMenu));
-// bars.addEventListener('click', hamFunction);
 
 function scrolling() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
